c90e989fd5433376554c026632d2dee4b4e29f7e
SolarLune <solarlune@gmail.com>
2023-01-25 19:06:51 -0800

    Reverting the previous changes; this just makes things cleaner.

--- cando.go
+++ cando.go
@@ -1,12 +1,10 @@
 package cando
 
-import "fmt"
-
 // IState is a basic interface that works like you would expect.
-type IState interface {
-	Enter()
-	Update()
-	Exit()
+type State struct {
+	Enter  func()
+	Update func()
+	Exit   func()
 }
 
 // TODO: Add something to define and return if states can enter into other ones.
@@ -14,27 +12,29 @@ type IState interface {
 // FSM represents a Finite State Machine, which can have one State active at a time.
 type FSM struct {
 	CurrentState string
-	States       map[string]IState
+	States       map[string]State
 }
 
 // NewFSM creates a new FSM and returns it.
 func NewFSM() *FSM {
 	fsm := &FSM{}
-	fsm.States = make(map[string]IState, 0)
+	fsm.States = make(map[string]State, 0)
 	return fsm
 }
 
 // Update runs the Update() on the active State.
 func (f *FSM) Update() {
-	if f.CurrentState != "" {
+
+	if f.CurrentState != "" && f.States[f.CurrentState].Update != nil {
 		f.States[f.CurrentState].Update()
 	} else {
-		fmt.Println("Update() called on FSM without active state.")
+		panic("Update() called on FSM without an active state")
 	}
+
 }
 
 // Register registers a State with its name.
-func (f *FSM) Register(name string, state IState) {
+func (f *FSM) Register(name string, state State) {
 	f.States[name] = state
 }
 
@@ -51,9 +51,9 @@ func (f *FSM) HasState(name string) bool {
 
 // Change allows you to change the current, "main" State assigned to the FSM. If you run Change(), it will call
 // Exit() on the previous State and Enter() on the next State.
-func (f *FSM) Change(stateName string) {
+func (f *FSM) Change(stateName string) State {
 
-	if f.CurrentState != "" {
+	if f.CurrentState != "" && f.States[f.CurrentState].Exit != nil {
 		f.States[f.CurrentState].Exit()
 	}
 
@@ -67,5 +67,7 @@ func (f *FSM) Change(stateName string) {
 	if f.CurrentState != "" && f.States[f.CurrentState].Enter != nil {
 		f.States[f.CurrentState].Enter()
 	}
+
+	return f.States[f.CurrentState]
 
 }
--- example/main.go
+++ example/main.go
@@ -10,59 +10,61 @@ import (
 
 // -------------------------------
 
-type IdleState struct {
-	Human *Human
-}
+func IdleState(human *Human) cando.State {
 
-func (state IdleState) Enter() {}
-func (state IdleState) Update() {
+	return cando.State{
 
-	state.Human.Hunger -= 10
+		Update: func() {
 
-	hunger := state.Human.Hunger
+			human.Hunger -= 10
 
-	fmt.Println("My hunger level is at: ", state.Human.Hunger)
+			fmt.Println("My hunger level is at: ", human.Hunger)
 
-	if hunger <= 20 {
-		fmt.Println("I've reached my limit; I'm getting something to eat.")
-		state.Human.FSM.Change("search")
+			if human.Hunger <= 20 {
+				fmt.Println("I've reached my limit; I'm getting something to eat.")
+				human.FSM.Change("search")
+			}
+
+		},
 	}
 
 }
-func (state IdleState) Exit() {}
 
 // -------------------------------
 
-type SearchState struct {
-	Human *Human
-}
+func SearchState(human *Human) cando.State {
 
-func (state SearchState) Enter() {}
-func (state SearchState) Update() {
-	if rand.Float32() < 0.2 {
-		fmt.Println("Ah, found something to eat!")
-		state.Human.FSM.Change("eating")
-	} else {
-		fmt.Println("Hmm... I searched, but there wasn't anything to eat.")
+	return cando.State{
+
+		Update: func() {
+			if rand.Float32() < 0.2 {
+				fmt.Println("Ah, found something to eat!")
+				human.FSM.Change("eating")
+			} else {
+				fmt.Println("Hmm... I searched, but there wasn't anything to eat.")
+			}
+		},
 	}
+
 }
-func (state SearchState) Exit() {}
 
 // -------------------------------
 
-type EatingState struct {
-	Human *Human
-}
+func EatingState(human *Human) cando.State {
 
-func (state EatingState) Enter() { fmt.Println("Finally, some good grub!") }
-func (state EatingState) Update() {
-	state.Human.Hunger += 10
-	fmt.Println("*Chomp* *Smack* : ", state.Human.Hunger)
-	if state.Human.Hunger >= 100 {
-		state.Human.FSM.Change("idle")
+	return cando.State{
+		Enter: func() { fmt.Println("Finally, some good grub!") },
+		Update: func() {
+			human.Hunger += 10
+			fmt.Println("*Chomp* *Smack* : ", human.Hunger)
+			if human.Hunger >= 100 {
+				human.FSM.Change("idle")
+			}
+		},
+		Exit: func() { fmt.Println("Phew, that was good. Back to doing nothing.") },
 	}
+
 }
-func (state EatingState) Exit() { fmt.Println("Phew, that was good. Back to doing nothing.") }
 
 // -------------------------------
 
@@ -79,9 +81,9 @@ func NewHuman() *Human {
 		FSM:    cando.NewFSM(),
 	}
 
-	human.FSM.Register("idle", IdleState{Human: human})
-	human.FSM.Register("search", SearchState{Human: human})
-	human.FSM.Register("eating", EatingState{Human: human})
+	human.FSM.Register("idle", IdleState(human))
+	human.FSM.Register("search", SearchState(human))
+	human.FSM.Register("eating", EatingState(human))
 	human.FSM.Change("idle")
 
 	return human
